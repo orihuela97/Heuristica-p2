0a2d2746650e3c9f80fd1db4808e3a97080c4235
100383366 <100383366@alumnos.uc3m.es>
2019-11-15 20:33:36 +0100

    salvy9978 15/11/2019 20:33

--- CSPScheduling.py
+++ CSPScheduling.py
@@ -1,6 +1,6 @@
 from constraint import *
 
-def out_file(solution, solution2):
+def out_file(solution):
 	archivo = open("output.txt", "w")
 	frase = "Solucion al problema 1 obtenida correctamente.\n"
 	archivo.write(frase)
@@ -25,146 +25,85 @@ def out_file(solution, solution2):
 	archivo.write(frase)
 	frase = "-------------------------------------------------------------------------\n"
 	archivo.write(frase)
-	frase = solution2.get("N") + "             " + solution2.get("S") + "             " + solution2.get("M") +  "              " + solution2.get("L") + "              " + solution2.get("I")+ "              " + solution2.get("F") + "\n"
+	frase = solution.get("Naturales") + "             " + solution.get("Sociales") + "             " + solution.get("Mates") +  "              " + solution.get("Lengua") + "              " + solution.get("Ingles")+ "              " + solution.get("Gimnasia") + "\n"
 	archivo.write(frase)
 	archivo.close()
 
-def two_perTeacher(*args):
-	juan = 0
-	andrea = 0
-	lucia = 0
-	juanList = []
-	andreaList = []
-	luciaList = []
+
+def totalHoras(*args):
+	switcher = {'Naturales':0,'Sociales':0,'Lengua':0,'Ingles':0,'Mates':0,'Gimnasia':0}
 	for i in args:
-		if i == 'J':
-			juan += 1
-		elif i == 'L':
-			lucia += 1
-		else:
-			andrea += 1
-	if juan == 2 and andrea == 2 and lucia == 2:
+		switcher[i]=switcher[i]+1;
+	if switcher['Naturales'] == 2 and switcher['Sociales'] == 2 and switcher['Lengua'] == 2 and switcher['Mates'] == 2 and switcher['Ingles'] == 2 and switcher['Gimnasia'] == 1:
+		return True
+
+def naturalesSameDay(*args):
+	for i in range(len(args)):
+		if(args[i]=='Naturales'):
+			if(i+1>=len(args) or args[i+1]!='Naturales'):
+				return False
+			else:
+				return True
+	return True
+
+def Math_NatIngles(*args):
+	hayMates = 0
+	hayNaturales_o_Ingles = 0
+	for i in args:
+		if i == 'Mates':
+			hayMates = 1
+		if i=='Naturales' or i=='Ingles':
+			hayNaturales_o_Ingles = 1
+	if hayMates+hayNaturales_o_Ingles == 2:
+			return False
+	return True
+
+def two_perTeacher(*args):
+	switcher = {'Juan':0,'Andrea':0,'Lucia':0}
+	for i in args:
+		switcher[i] = switcher[i]+1;
+	if switcher['Juan'] == 2 and switcher['Andrea'] == 2 and switcher['Lucia'] == 2:
 		return True
 
 def lucia_Sociales(*args):
-	if args[0] == 'L':
-		if args [1] == 'A':
-			return True
-	else:
-		return True
-def juan_NaturalesSociales(*args):
-	acepta = 0
-	if args[0] == 'J':
-		if args[1] != 'N' and args[2] != 'N':
-			acepta = 1
-	if acepta == 1:
-		return True
-def totalHours(*args):
-	contadorNaturales = 0
-	contadorSociales = 0
-	contadorLengua = 0
-	contadorMatematicas = 0
-	contadorIngles = 0
-	contadorEfisica = 0
-	for i in args:
-		if i == 'N':
-			contadorNaturales += 1
-		if i == 'S':
-			contadorSociales += 1
-		if i == 'L':
-			contadorLengua += 1
-		if i == 'M':
-			contadorMatematicas += 1
-		if i == 'I':
-			contadorIngles += 1
-		if i == 'F':
-			contadorEfisica += 1
-	if contadorNaturales == 2 and contadorSociales == 2 and contadorLengua == 2 and contadorMatematicas == 2 and contadorIngles == 2 and contadorEfisica == 1:
-		return True
-def Math_NatIngles(*args):
-	hayMates = 0
-	hayNaturales = 0
-	hayIngles = 0
-	for i in args:
-		if i == 'M':
-			hayMates = 1
-	if hayMates == 1:
-		for i in args:
-			if i == 'N':
-				hayNaturales = 1
-			if i == 'I':
-				hayIngles = 1
-		if hayNaturales == 0 and hayIngles == 0:
-			return True
-	else:
-	 return True
-def MathFirstHours(*args):
-	hayMates = 0
-	for i in args:
-		if i == 'M':
-			hayMates = 1
-	if hayMates == 0:
-		return True
-def socialesLastHours(*args):
-	haySociales = 0
-	for i in args:
-		if i == 'S':
-			haySociales = 1
-	if haySociales == 0:
-		return True
-def naturalesSameDay(*args):
-	"""hayNaturales = 0
-	contadorNaturales = 0
-	for i in args:
-		if i == 'N':
-			hayNaturales = 1
-	if hayNaturales == 1:
-		for i in args:
-			if i == 'N':
-				contadorNaturales += 1
-		if contadorNaturales == 2:
-			return True
-	else:
-		return True"""
-	true = 0
-	hayNaturales = 0
-	for i in args:
-		if i == 'N':
-			hayNaturales = 1
-	if hayNaturales == 1:
-		for i in range(len(args)):
-			if args[i] == 'N' and ((i + 1) < len(args)):
-				if args[i+1] == 'N':
-					true = 1
-		if true == 1:
+	if args[0] == 'Lucia':
+		if args [1] == 'Andrea':
 			return True
 	else:
 		return True
 
+def juan_Lun_Jue_PrimeraNaturales(*args):
+	if args[0] == 'Juan' and (args[1]=='Naturales' or args[2]=='Naturales'):
+		return False
+	return True
+def juan_Lun_Jue_PrimeraSociales(*args):
+	if args[0] == 'Juan' and (args[1]=='Sociales' or args[2]=='Sociales'):
+		return False
+	return True
 
 problem = Problem()
-# Variables para el primer problema
-problem.addVariables(["L1", "L2", "L3","M1","M2","M3","X1","X2","X3","J1","J2"], ['N','S','L','M','I','F'])
-# Restricciones para el problema 1
+# Variables para el primer problema y cubro la restriccion mates solo a primera y sociales solo a segunda con el dominio
+problem.addVariables(["L1","M1","X1","J1"], ['Naturales','Lengua','Mates','Ingles','Gimnasia'])
+problem.addVariables(["L2","M2","X2"], ['Naturales','Lengua','Ingles','Gimnasia'])
+problem.addVariables(["L3","M3","X3","J2"], ['Naturales','Sociales','Lengua','Ingles','Gimnasia'])
+# Variables para el segundo problema
+problem.addVariables(['Naturales','Lengua','Mates','Ingles','Gimnasia','Sociales'],['Juan', 'Andrea', 'Lucia'])
+# Restricciones
+problem.addConstraint(totalHoras,('L1', 'L2', 'L3','M1','M2','M3','X1','X2','X3','J1','J2'))
 problem.addConstraint(naturalesSameDay, ('L1', 'L2', 'L3'))
 problem.addConstraint(naturalesSameDay, ('M1', 'M2', 'M3'))
 problem.addConstraint(naturalesSameDay, ('X1', 'X2', 'X3'))
 problem.addConstraint(naturalesSameDay, ('J1', 'J2'))
-problem.addConstraint(socialesLastHours,("L1", "L2","M1","M2","X1","X2","J1"))
-problem.addConstraint(MathFirstHours, ("L2", "L3","M2","M3","X2","X3","J2"))
 problem.addConstraint(Math_NatIngles, ('L1', 'L2', 'L3'))
 problem.addConstraint(Math_NatIngles, ('M1', 'M2', 'M3'))
 problem.addConstraint(Math_NatIngles, ('X1', 'X2', 'X3'))
 problem.addConstraint(Math_NatIngles, ('J1', 'J2'))
-problem.addConstraint(totalHours, ('L1', 'L2', 'L3','M1','M2','M3','X1','X2','X3','J1','J2'))
-#sSolucion al problema 1
+problem.addConstraint(two_perTeacher, ('Naturales','Lengua','Mates','Ingles','Gimnasia','Sociales'))
+problem.addConstraint(lucia_Sociales, ('Sociales', 'Gimnasia'))
+problem.addConstraint(juan_Lun_Jue_PrimeraNaturales, ('Naturales', 'L1', 'J1'))
+problem.addConstraint(juan_Lun_Jue_PrimeraSociales, ('Sociales', 'L1', 'J1'))
+
+#Solucion
 solution = problem.getSolution()
-#-------------------------------------------------------------------------PROBLEMA2-----------------------------
-problem2 = Problem()
-problem2.addVariables(['N','S','L','M','I','F'],['J', 'A', 'L'])
-problem2.addConstraint(juan_NaturalesSociales, ('N', solution.get("L1"), solution.get("J1")))
-problem2.addConstraint(lucia_Sociales, ('S', 'F'))
-problem2.addConstraint(two_perTeacher, ('N','S','L','M','I','F'))
-solution2 = problem2.getSolution()
 #------------------------------------------------SALIDA A UN ARCHIVO DE TEXTO--------------------------------
-out_file(solution, solution2)
+out_file(solution)
