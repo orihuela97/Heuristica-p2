f2f3b3690b5c26b8fc6fcb144ca1999fb8fcc5e9
100383366 <100383366@alumnos.uc3m.es>
2019-12-11 01:35:19 +0100

    v1

--- a_star.py
+++ a_star.py
@@ -1,47 +1,63 @@
-from parser import *
-
-class Node:
-    def __init__(self,pBus,listaAlumnosSubidos,listaAlumnosPendientes):
-        self.pBus = pBus
-        self.listaAlumnosSubidos = listaAlumnosSubidos
-        self.listaAlumnosPendientes = listaAlumnosPendientes
-
-
-
-
-
-
-
-
+from parserDatos import *
 
 
 
 arguments = reader("input.txt")
+
+
+
+def compararEstados(estado1,estado2): #devuelve True si son iguales
+    pBus1=estado1[0]
+    pBus2=estado2[0]
+    listAlumSub1=estado1[1]
+    listAlumSub2=estado2[1]
+    listAlumPend1=estado1[2]
+    listAlumPend2=estado2[2]
+    if(pBus1!=pBus2 or len(listAlumSub1)!=len(listAlumSub2) or len(listAlumPend1)!=len(listAlumPend2)):
+        return False;
+    for i in listAlumSub1:
+        if (i in listAlumSub2)==False:
+            return False
+    for i in listAlumPend1:
+        if (i in listAlumPend2)==False:
+            return False
+    return True
+
+
 #Configuracion
 matrizCostes=arguments[3]
-capacidadBus=arguments[0].get("Capacidad")
+capacidadBus=arguments[0].get("capacidad")
 listaColegios=arguments[1]
-id=1
+costeCargaPorAlumno=1
+costeDescargaPorAlumno=1
+
 #nodo inicial
-estadoInicial = Node(]arguments[0].get("Parada"),[],arguments[2])
+estadoInicial = [arguments[0].get("parada"),[],arguments[2]] #posicionBus, listaAlumnosSubidos, listaAlumnosPendientes
 
 #nodo meta
-estadoFinal = Node(arguments[0].get("Parada"),[],[])
+estadoFinal = [arguments[0].get("parada"),[],[]]
 
 #funcion heuristica
-def heuristica(estado):
+def funcionHeuristica(estado):
     return 0
 
 
 #acciones
 def operadores(nodo,listaAbierta,listaCerrada):
-    filaCostes = matrizCostes[nodo[2][0]-1]
+    sucesores=[]
+    #desplazamiento
+    filaCostes = matrizCostes[nodo[1][0]-1]
     for i in range(len(filaCostes)):
         if(filaCostes[i]!=-1):
-            estadoGenerado=[i+1,nodo[2][1],nodo[2][2]]
-            id=id+1
-            nodoGenerado=[id,nodo[0],estadoGenerado,nodo[3]+filaCostes[i],heuristica(estadoGenerado)]
-            listaAbierta.append(nodoGenerado)
+            estadoGenerado=[i+1,nodo[1][1],nodo[1][2]]
+            coste = nodo[2]+filaCostes[i]
+            heuristica = funcionHeuristica(estadoGenerado)
+            nodoGenerado=[nodo[1],estadoGenerado,coste,coste+heuristica]
+            sucesores.append(nodoGenerado)
+    #cargar
+    
+
+    #descargar
 
 
 
@@ -51,42 +67,18 @@ def operadores(nodo,listaAbierta,listaCerrada):
 listaAbierta=[]
 listaCerrada=[]
 exito=False
+nodoInicial=[[],estadoInicial,0,funcionHeuristica(estadoInicial)]
+listaAbierta.append([[],estadoInicial,0,funcionHeuristica(estadoInicial)]) #estadoNodoPadre, estadoNodo, coste, funcionEvaluacion
 
 
-listaAbierta.append([id,1,estadoInicial,0,heuristica(estadoInicial)]) #idNodo, idPadre, nodo, coste
-while len(listaAbierta)!=0 and exito==False:
+while len(listaAbierta)!=0 and not exito:
     nodoAExpandir=listaAbierta.pop(0)
     nodoEncontrado=False
-    for i in listaCerrada and nodoEncontrado==False:
-        if i[2]==nodoAExpandir[2]:
-            nodoEncontrado=True
+    for i in listaCerrada and not nodoEncontrado:
+            nodoEncontrado=compararEstados(i[1],nodoAExpandir[1])
 
-    if(nodoEncontrado==False):
-        if nodoAExpandir[2]==estadoFinal:
+    if(not nodoEncontrado):
+        if compararEstados(nodoAExpandir[1],estadoFinal):
             exito=True
         else:
-            print(nodoAExpandir)
             operadores(nodoAExpandir,listaAbierta,listaCerrada)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-print(arguments[0])
-print(arguments[1])
-print(arguments[2])
-print(arguments[3])
--- parserDatos.py
+++ parserDatos.py
@@ -8,47 +8,42 @@ def leeBusData(lista):
         if i != "P":
             aux += i
     #BUS ES UN DICCIONARIO#
-    bus = dict(Parada = int(aux),Capacidad = int(lista[1]))
+    bus = dict(parada = int(aux),capacidad = int(lista[1]))
     return bus
 
 def leeColegios(lista):
-
     colegios = []
     lista = lista.split(";")
     aux = []
     aux1 = ""
     idColegio = ""
-    Parada = ""
+    parada = ""
     for i in lista:
         aux1 = ""
         aux = i.split(":")
         for j in aux[0]:
-            if j != "C":
+            if j != "C" and j != " ":
                 aux1 += j
         idColegio = aux1
         aux1 = ""
         for j in aux[1]:
             if j != "P" and j != " ":
                 aux1 += j
-        Parada = aux1
-        diccionario_aux = dict(Colegio = int(idColegio), numParada = int(Parada))
-        colegios.append(diccionario_aux)
+        parada = aux1
+        colegio = [idColegio,parada]
+        colegios.append(colegio)
 
     return colegios
 
 def leerAlumnos(lista):
     lista = lista.split(";")
-
-    paradas = []
     alumnos = []
     for i in lista:
-
-
         j = 0
         puntero = i[j]
         numParada = ""
         while puntero != ":":
-            if puntero != "P":
+            if puntero != "P" and puntero != " ":
                 numParada += puntero
             j += 1
             puntero = i[j]
@@ -57,21 +52,15 @@ def leerAlumnos(lista):
         x = i.split(":")
         x.pop(0)
         x = x[0].split(",")
-        paradaActual = 0
-        for z in x:
 
+        for z in x:
             z = z.split("C")
             numAlumnos = int(z[0])
-            numColegio = int(z[1])
+            idColegio = int(z[1])
             for k in range(numAlumnos):
-                diccionario_aux = dict(Colegio = numColegio, ParadaOrigen = numParada)
-                alumnos.append(diccionario_aux)
-        paradaActual += 1
+                alumno=[idColegio,numParada]
+                alumnos.append(alumno)
     return alumnos
-
-
-
-
 
 
 
@@ -81,50 +70,43 @@ def reader(fichero):
     f = open(fichero)
     x = f.read()
     x= x.split("\n")
-    y = x.pop(len(x)-2)
-    #BUS ES UN DICCIONARIO CON {PARADA,CAPACIDAD}
-    bus = leeBusData(y)
-
-
-    y = x.pop(len(x)-3)
-    #COLEGIOS ES UNA LISTA DE DICCIONARIOS, CADA DICCIONARIO TIENE: {IDCOLEGIO,NUMPARADA}
-    colegios = leeColegios(y)
-
-    y = x.pop(len(x)-2)
-    alumnos = leerAlumnos(y)
-
+    aux=[]
+    for i in range(len(x)):
+        if(x[i]!=''):
+            aux.append(x[i])
+    x=aux
     y = x.pop(0)
     y = y.split(" ")
     aux = []
-    aux2 = []
     costMatrix = []
+
     for i in range(len(y)):
         if(y[i] != ''):
             aux.append(y[i])
-    nParadas = len(aux)
+    y=aux
+    nParadas = len(y)
     for i in range(nParadas):
-        aux2=[]
+        aux=[]
         aux1 = x.pop(0)
         aux1 = aux1.split(" ")
         aux1.pop(0)
         for j in range(len(aux1)):
             if aux1[j] != '':
                 if  aux1[j] == '--':
-                    aux2.append(-1)
+                    aux.append(-1)
                 else:
                     try:
-                        aux2.append(int(aux1[j]))
-
+                        aux.append(int(aux1[j]))
                     except:
                         print("La variable "+ aux1[j]+ " no es un entero")
                         sys.exit()
 
-        costMatrix.append(aux2)
-
-
+        costMatrix.append(aux)
+    colegios = leeColegios(x.pop(0))
+    alumnos = leerAlumnos(x.pop(0))
+    bus = leeBusData(x.pop(0))
     arguments.append(bus)
     arguments.append(colegios)
     arguments.append(alumnos)
     arguments.append(costMatrix)
     return arguments
-
